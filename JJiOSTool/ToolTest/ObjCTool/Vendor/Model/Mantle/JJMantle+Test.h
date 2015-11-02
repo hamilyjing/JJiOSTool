d40e2b947d8ee387cfcdec5490025921ac0110b5
JJ <gongjian_001@126.com>
2015-11-02 11:26:02 +0800

    jsonmodel and mantle

---
+++ JJiOSTool/ToolTest/ObjCTool/Vendor/Model/Mantle/JJMantle+Test.h
@@ -0,0 +1,11 @@
+//
+//  JJMantle+Test.h
+//  JJObjCTool
+//
+//  Created by JJ on 5/23/15.
+//  Copyright (c) 2015 gongjian. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+extern void jjMantleTest();
